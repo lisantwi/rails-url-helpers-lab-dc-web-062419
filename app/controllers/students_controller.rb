88a3a32673bb4d880750a01ba2e3baf27f6fa0c3
Lisa Antwi Adjei <lisaantwi@gmail.com>
2019-07-22 02:54:48 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,6 +10,10 @@ class StudentsController < ApplicationController
   end
 
   def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
